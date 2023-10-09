ac9bfb2a211d775935a7ec463a66482e1657c388
Muhammad ryhan <mhmdryhan02@gmail.com>
2023-10-10 02:09:43 +0800

    flask setup for deployment

--- app.py
+++ app.py
@@ -1,14 +1,41 @@
+import os
+from os.path import join, dirname
+from dotenv import load_dotenv
+
 from flask import Flask, render_template, request, jsonify
 from pymongo import MongoClient
 
-client = MongoClient('mongodb+srv://mryhan:mryhan@cluster0.cq5ursx.mongodb.net/?retryWrites=true&w=majority&appName=AtlasApp')
-db = client.dbmryhan
+dotenv_path = join(dirname(__file__), '.env')
+load_dotenv(dotenv_path)
+
+MONGODB_URI = os.environ.get("MONGODB_URI")
+DB_NAME =  os.environ.get("DB_NAME")
+
+client = MongoClient(MONGODB_URI)
+db = client[DB_NAME]
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
     return render_template('index.html')
+
+@app.route("/fanmessages", methods=["POST"])
+def homework_post():
+    # sample_receive = request.form['sample_give']
+    nn_receive = request.form['name_give']
+    message_receive = request.form['comment_give']
+    doc = {
+        'name': nn_receive,
+        'comment': message_receive,
+    }
+    db.fanmessages.insert_one(doc)
+    return jsonify({'msg':'Comment Posted!'})
+
+@app.route("/fanmessages", methods=["GET"])
+def homework_get():
+    message_list = list(db.fanmessages.find({}, {'_id': False}))
+    return jsonify({'messages': message_list})
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
