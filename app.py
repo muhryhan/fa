0c2c4d6cf916b1606583de22d49d102dd65308c6
Muhammad ryhan <mhmdryhan02@gmail.com>
2023-10-10 00:11:53 +0800

    Perubahan kode app.py

--- app.py
+++ app.py
@@ -10,41 +10,5 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
-@app.route("/bucket", methods=["POST"])
-def bucket_post():
-    bucket_receive = request.form["bucket_give"]
-    count = db.bucket.count_documents({})
-    num = count + 1
-    doc = {
-        'num':num,
-        'bucket': bucket_receive,
-        'done':0
-    }
-    db.bucket.insert_one(doc)
-    return jsonify({'msg':'data saved!'})
-
-@app.route("/bucket/done", methods=["POST"])
-def bucket_done():
-    num_receive = request.form["num_give"]
-
-    db.bucket.update_one(
-        {'num': int(num_receive)},
-        {'$set': {'done': 1}}
-    )
-    return jsonify({'msg': 'Update done!'})
-
-@app.route("/bucket", methods=["GET"])
-def bucket_get():
-    bucket_list = list(db.bucket.find({}, {'_id': False}))
-    return jsonify({'buckets': bucket_list})
-
-@app.route("/delete", methods=["POST"])
-def bucket_delete():
-    num_receive = request.form["num_give"]
-
-    db.bucket.delete_one({'num': int(num_receive)})
-
-    return jsonify({'msg': 'Item deleted!'})
-
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
